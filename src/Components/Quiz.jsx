a1356b87994f3a915d78a1fc3927cbaf456588c9
Anjith Vasam <159884936+ANJITHVASAM@users.noreply.github.com>
2026-03-04 04:09:18 +0530

    Update Quiz.jsx

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -4,7 +4,7 @@ import "./components.css";
 function Quiz({ language ,questions, answers, setAnswers, submitted }) {
   return (
     <div>
-      <h1 className="main-heading">You Choosed <b className="main-heading-bold">{language}</b></h1>
+      <h1 className="main-heading">You Chose <b className="main-heading-bold">{language}</b></h1>
       {questions.map((q, index) => (
         <QuestionCard 
           key={q.id} 
