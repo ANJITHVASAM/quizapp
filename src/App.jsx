ee85ec583dc4f6fce439968f1cdbc697607de126
Anjith Vasam <159884936+ANJITHVASAM@users.noreply.github.com>
2026-06-18 20:44:44 +0530

    Add noOfQuestions state and pass to routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,32 @@ import { useEffect, useState } from "react";
 function App() {
   const [language, setLanguage] = useState("");
   const [level, setLevel] = useState("");
+  const [noOfQuestions,setNoOfQuestions]=useState("");
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage level={level} setLevel={setLevel} language={language} setLanguage={setLanguage} />} />
-        <Route path="/quiz" element={<StartQuiz level={level} language={language} />} />
+        <Route 
+          path="/" 
+          element={
+            <HomePage 
+              level={level} 
+              setLevel={setLevel} 
+              language={language} 
+              setLanguage={setLanguage} 
+              noOfQuestions={noOfQuestions}
+              setNoOfQuestions={setNoOfQuestions}
+            />} 
+        />
+        <Route 
+          path="/quiz" 
+          element={
+            <StartQuiz 
+              level={level} 
+              language={language} 
+              noOfQuestions={noOfQuestions}
+              setNoOfQuestions={setNoOfQuestions}
+            />} 
+        />
       </Routes>
     </BrowserRouter>
   );
