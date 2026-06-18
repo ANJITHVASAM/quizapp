cd4a3d18261f0cc54c5b4c87de714fd30671e761
Anjith Vasam <159884936+ANJITHVASAM@users.noreply.github.com>
2026-06-18 20:45:37 +0530

    Enhance HomePage with question selection feature
    Added functionality to select the number of questions for the quiz.

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import "./components.css";
 
-function HomePage({level, setLevel, language, setLanguage}) {
+function HomePage({level, setLevel, language, setLanguage, noOfQuestions, setNoOfQuestions }) {
 
   return (
     <div className="container">
       <h1 className="main-heading">Student Quiz App</h1>
+      <div class="hero">
+        <div class="hero-badge">🧑‍💻 9 Programming Languages</div>
+        <h1 class="hero-h1">Level Up Your<span class="g">Coding Skills</span></h1>
+        <p class="hero-p">Test yourself across HTML, CSS, JavaScript, Java, Python, SQL, C, React & PHP. Real questions. Three difficulty levels.</p>
+      </div>
       <div className="options">
         <h2 className="question-title">Choose The Language</h2>
         <select
@@ -33,6 +38,15 @@ function HomePage({level, setLevel, language, setLanguage}) {
           <option value="medium">Medium</option>
           <option value="hard">Hard</option>
         </select>
+        <h2 className="question-title">Select Questions</h2>
+        <input 
+          className="selectNoOfQuestions" 
+          type="number" 
+          defaultValue={5} 
+          min={1} 
+          max={40}
+          onChange={(e) => setNoOfQuestions(e.target.value)}
+        />
         <br />
         <Link to="/quiz">
             <button className="button" disabled={!level || !language}>Start Quiz</button>
